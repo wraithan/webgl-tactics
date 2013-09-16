b7addc65c3c5c14a0e2b729a8ca660bad6bca5d2
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-09-15 20:13:20 -0700

    map generator and loader

--- index.js
+++ index.js
@@ -1,39 +1,40 @@
 var THREE = require('three')
+  , tile = require('./lib/tile')
+  , fps = require('./lib/fps')
+  , generator = require('./lib/map-generator')
+  , loader = require('./lib/map-loader')
 
 window.onload = function() {
   var WIDTH = 400
     , HEIGHT = 300
     , container = document.getElementById('container')
-    , fpsElement = document.getElementById('fps')
     , renderer = new THREE.WebGLRenderer()
     , camera = new THREE.OrthographicCamera(WIDTH/-2, WIDTH/2,
                                             HEIGHT/2, HEIGHT/-2)
     , scene = new THREE.Scene()
+    , fpsElement = document.getElementById('fps')
+    , trackFps = fps(function(FPS) {
+      fpsElement.textContent = 'FPS: ' + FPS
+    })
 
   scene.add(camera)
 
   camera.position.x = 0
-  camera.position.y = 45
-  camera.position.z = 100
+  camera.position.y = 165
+  camera.position.z = 330
 
   renderer.setSize(WIDTH, HEIGHT)
 
   container.appendChild(renderer.domElement)
 
-  var cubeMaterial = new THREE.MeshLambertMaterial({
-      color: 0x00FFFF
-    , ambient: 0x00FFFF
-    })
-    , cube = new THREE.Mesh(new THREE.CubeGeometry(60, 30, 60),
-                              cubeMaterial)
+  var map = generator(10, 9, 5)
+  loader(scene, map)
 
-  scene.add(cube)
+  var pointLight = new THREE.PointLight(0xFFFFFF, 0.5)
 
-  var pointLight = new THREE.PointLight(0xFFFFFF, 1)
-
-  pointLight.position.x = 60
-  pointLight.position.y = 60
-  pointLight.position.z = 60
+  pointLight.position.x = 200
+  pointLight.position.y = 100
+  pointLight.position.z = 200
 
   scene.add(pointLight)
 
@@ -41,27 +42,15 @@ window.onload = function() {
 
   scene.add(ambientLight)
 
-  var theta = 0.1
+  var theta = 0.01
   setInterval(function() {
     var x = camera.position.x
       , z = camera.position.z
 
     camera.position.x = x * Math.cos(theta) + z * Math.sin(theta)
     camera.position.z = z * Math.cos(theta) - x * Math.sin(theta)
-    camera.lookAt(cube.position)
+    camera.lookAt({x: 0, y: 0, z: 0})
   }, 1000/60)
-
-  var frame = 0
-    , sample = 0
-  function trackFps(time) {
-    var delta = time-sample
-    frame++
-    if (delta > 1000) {
-      fpsElement.textContent = 'FPS: ' + (frame/(delta/1000)).toFixed(2)
-      sample = time
-      frame = 0
-    }
-  }
 
   ;(function animloop(time) {
     window.requestAnimationFrame(animloop)
